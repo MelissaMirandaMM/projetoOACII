6a1dad01cc5178c3d53217093785c5e87e768eb4
Melissa Miranda <melissamiranda.14.mm@gmail.com>
2023-03-14 14:02:38 -0300

    ULA e MIPS

--- MIPS.v
+++ MIPS.v
@@ -10,3 +10,44 @@ module MIPS(input             clk,
 	
 
 endmodule
+
+always @(*) begin
+    case(opALU)
+        2'b00: begin //load word or store word
+            if (funct[4:0] == 6'b100011) //lw
+                outputULA = 32'b00000000000000000000000000000011;
+            else if (funct[4:0] == 6'b101011) //sw
+                outputULA = 32'b00000000000000000000000000000011;
+        end
+        // add, subtract, and, or, set on less than
+        // code for other operations
+    endcase
+end
+
+else if (opALU == 2'b01) begin //branch equal
+    if (a == b) 
+        outputULA = 1;
+    else
+        outputULA = 0;
+end
+
+else if (opALU == 2'b10) begin //add
+    outputULA = a + b;
+end
+
+else if (opALU == 2'b10) begin //subtract
+    outputULA = a - b;
+end
+
+else if (opALU == 2'b10) begin //and
+    outputULA = a & b;
+end
+
+else if (opALU == 2'b10) begin //or
+    outputULA = a | b;
+end
+
+else if (opALU == 2'b10) begin //set on less than
+    if (a < b)
+        outputULA = 1;
+    else
